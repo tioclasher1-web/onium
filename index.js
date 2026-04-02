93f48ad14cb851f8a8b6771144ae12b00d2df43c
ONIUM RACE <tioclasher1@gmail.com>
2026-04-02 23:27:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Onium Race - Key Management System</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="https://files.catbox.moe/bjwzg5.png">
-<link rel="shortcut icon" href="https://files.catbox.moe/bjwzg5.png">
+    <link rel="icon" href="https://files.catbox.moe/bjwzg5.png?v=2">
     <style>
         * {
             margin: 0;
